537a74a0dcf7ae16d055853b4f2b8ba34022cbb8
Md Fahim Al Azad <fahimsami91@gmail.com>
2026-05-03 22:31:52 +0600

    Displaying email is added

--- FreeCodeCamp/Email Simulator/Email_Simulator.py
+++ FreeCodeCamp/Email Simulator/Email_Simulator.py
@@ -9,9 +9,27 @@ class Email:
     def mark_as_read(self):
         self.read = True
         
+    def display_email(self):
+        self.mark_as_read()
+        print("\n----- Email -----")
+        print(f"From: {self.sender.name}")
+        print(f"To : {self.receiver.name}")
+        print(f"Subject: {self.subject}")
+        print(f"Body : {self.body}")
+        print("------------------\n")
+        
 class User:
     def __init__(self, name):
         self.name = name
+        self.inbox = Inbox()
         
     def send_email(self, receiver, subject, body):
+        email = Email(sender=self, receiver = receiver, subject = subject, body = body)
+        receiver.inbox.receive_email(email)
+class Inbox():
+    def __init__(self):
+        self.emails = []
+        
+    def receive_email(self, email):
+        self.emails.append(email)
         
