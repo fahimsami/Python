1d7a8b84cded2f7ad274efb0405553dab3157dfe
Md Fahim Al Azad <fahimsami91@gmail.com>
2026-05-07 18:26:56 +0600

    Main function edited

--- Lan Python/Polygon Area Calculator/Polygon.py
+++ Lan Python/Polygon Area Calculator/Polygon.py
@@ -70,22 +70,24 @@ class Square(Rectangle):
         
     def __str__(self):
         return f"Square (side = {self._width})"
-        
-rect = Rectangle(10, 5)
-print(rect.get_area())
-rect.set_height(3)
-print(rect.get_perimeter())
-print(rect)
-print(rect.get_picture())
-       
-sq = Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
-print(sq.get_picture())   
 
-rect.set_height(8)
-rect.set_width(16)
-print(rect.get_amount_inside(sq))     
+
+if __name__ == '__main__':        
+        rect = Rectangle(10, 5)
+        print(rect.get_area())
+        rect.set_height(3)
+        print(rect.get_perimeter())
+        print(rect)
+        print(rect.get_picture())
+       
+        sq = Square(9)
+        print(sq.get_area())
+        sq.set_side(4)
+        print(sq.get_diagonal())
+        print(sq)
+        print(sq.get_picture())   
+
+        rect.set_height(8)
+        rect.set_width(16)
+        print(rect.get_amount_inside(sq))     
         
